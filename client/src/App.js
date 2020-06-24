fd09027bad56b0b86b373cad9c3344999a787911
herrjosh <herrjosh@lifetime.oregonstate.edu>
2020-06-23 22:52:50 -0700

    added input for a todo item in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react/jsx-filename-extension */
+import React, { Fragment } from 'react';
 import './App.css';
+
+// components
+import InputTodo from './components/InputTodo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // eslint-disable-next-line react/jsx-fragments
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+
+      </div>
+    </Fragment>
   );
 }
 
