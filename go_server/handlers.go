76e863b3f09964a1a0a52f17e8f70ea648b127bf
herrjosh <herrjosh@lifetime.oregonstate.edu>
2021-02-27 10:22:20 -0800

    added cors handling and json response

--- go_server/handlers.go
+++ go_server/handlers.go
@@ -1,13 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request)  {
 	if r.URL.Path != "/" {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
-	data := []byte("Hello World!")
-
-	w.Write(data)
+	// data := []byte("Hello World!")
+	data := struct{
+		Message string `json:"message"`
+	}{"hello world!"}
+	w.WriteHeader(http.StatusAccepted)
+	w.Header().Set("Content-Type", "application/json")
+	// w.Write(data)
+	json.NewEncoder(w).Encode(data)
 }
